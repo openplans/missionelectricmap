5c52aa95f6dff469f6306e8b5b233db92238c0fe
Julia West <juliamae@gmail.com>
2012-04-25 17:34:20 -0400

    Getting the parent window script and shareabouts script communicating.

--- public/javascripts/missionelectric.js
+++ public/javascripts/missionelectric.js
@@ -1,1 +1,25 @@
+// subdomain XSF workaround
+document.domain = "openplans.org";
+
 if (window.console) window.console.log("hello from shareabouts");
+
+// marker click, show something in the popup
+
+$(function(){
+  $("#shareabouts iframe").ready(function(){
+    console.log("iframe loaded")
+    var intervalId = window.setInterval(function(){
+      try {
+        if (window.map.mapLoaded) {
+          window.mapWidget = window.map.embeddedMap;
+          console.log(window.mapWidget );      
+          window.clearInterval(intervalId);
+        };
+      } catch(err) {
+        window.clearInterval(intervalId);
+      }
+    },500);
+  });
+});
+
+
