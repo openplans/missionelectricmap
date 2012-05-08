6addef2d63d4f1704171dabc7526b08089952aeb
Julia West <juliamae@gmail.com>
2012-05-08 17:15:26 -0400

    Seeding location types; Displaying shadows on custom markers; Not using a 'focused' icon for current location marker.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,10 @@ for option_name in SiteOption::Names
   SiteOption.find_or_create_by_option_name option_name
 end
 
+["Mission Electric", "User-submitted"].each do |type|
+  LocationType.find_or_create_by_name type
+end
+
 # Create initial admin user
 Admin.create :email => "admin@example.com", :password => "p@ssw0rd", :password_confirmation => "p@ssw0rd", :level => 100
 puts "Created an initial user. Be sure to change admin user password!"
