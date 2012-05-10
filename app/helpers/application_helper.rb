1689a26ced5250a6a978bc84d620c23c64bb77ea
Julia West <juliamae@gmail.com>
2012-05-10 10:27:51 -0400

    Using full urls to images and resources.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,6 +36,14 @@ module ApplicationHelper
     end
   end
   
+  def image_url(source)
+    abs_path = image_path(source)
+    unless abs_path =~ /^http/
+      abs_path = "#{request.protocol}#{request.host_with_port}#{abs_path}"
+    end
+   abs_path
+  end
+  
   def page_link_attributes(page)
     page.welcome_page? ? {'data-welcome-page' => true} : {}
   end
